8e4b9bf4962e4d0e50f31cf701e39e2cb0ae4b08
Swaraj Rao <swaraj.rao@whoop.com>
2022-02-02 14:21:20 -0500

    checkout first, add logging

--- step.sh
+++ step.sh
@@ -3,13 +3,25 @@ set -e
 
 cd /Users/vagrant/git
 
+echo "git init"
 git init
 
+echo "git config --global url.'ssh://git@github.com'.insteadOf 'https://github.com' || true"
 git config --global url."ssh://git@github.com".insteadOf "https://github.com" || true
+
+echo "git config --global gc.auto 0 || true"
 git config --global gc.auto 0 || true
 
+echo "git remote add origin $repository_url"
 git remote add origin $repository_url
+
+echo "git clone -b $branch --no-tags --single-branch --depth=1 $repository_url"
 git clone -b $branch --no-tags --single-branch --depth=1 $repository_url
+
+echo "git checkout $branch"
+git checkout $branch
+
+echo "git merge origin/$branch_dest"
 git merge origin/$branch_dest
 
 envman add --key "GIT_CLONE_COMMIT_AUTHOR_NAME" --value "$(git "log" "-1" "--format=%an" $commit)"
