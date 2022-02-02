6715b70134df9f86421da6bbedf7ce2060aabff0
Swaraj Rao <swaraj.rao@whoop.com>
2022-02-02 17:02:18 -0500

    fetch before checkout

--- step.sh
+++ step.sh
@@ -20,19 +20,19 @@ git remote add origin $repository_url
 echo "$(date +"%H:%M:%S") - git clone --no-tags --single-branch --jobs=10 --branch=$branch $repository_url"
 git clone --no-tags --single-branch --jobs=10 --branch=$branch $repository_url
 
+echo "$(date +"%H:%M:%S") - git fetch --jobs=10 --no-tags origin $branch"
+git fetch --jobs=10 --no-tags origin $branch
+
 echo "$(date +"%H:%M:%S") - git checkout origin/$branch"
 git checkout origin/$branch
 
-echo "$(date +"%H:%M:%S") - FIRST_COMMIT=git log $branch_dest..$branch --oneline | tail -1 | awk '{print $1;}'"
+echo "$(date +"%H:%M:%S") - FIRST_COMMIT=git log --oneline | tail -1 | awk '{print $1;}'"
 FIRST_COMMIT=$(git log --oneline | tail -1 | awk '{print $1;}')
 
 echo "$(date +"%H:%M:%S") - FIRST_COMMIT_DATE=git show -s --format=%ci $FIRST_COMMIT --format=%as"
 FIRST_COMMIT_DATE=$(git show -s --format=%ci $FIRST_COMMIT --format=%as)
 
-echo "$(date +"%H:%M:%S") - git fetch origin $branch"
-git fetch --jobs=10 --shallow-since=$FIRST_COMMIT_DATE origin $branch
-
-echo "$(date +"%H:%M:%S") - git fetch origin $branch_dest"
+echo "$(date +"%H:%M:%S") - git fetch origin --jobs=10 --shallow-since=$FIRST_COMMIT_DATE $branch_dest"
 git fetch origin --jobs=10 --shallow-since=$FIRST_COMMIT_DATE $branch_dest
 
 echo "$(date +"%H:%M:%S") - git merge origin/$branch_dest"
